4005ac1643ea32e0a466cb1c10419d6b7ed3ee10
Steplon <43827083+Steplon@users.noreply.github.com>
2019-02-05 19:39:42 +0000

    DataTypes Testing

---
+++ WebApplication1/pages/DataType.cshtml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.pages
+{
+    [BindProperties]
+    public class DataTypeModel : PageModel
+    {
+
+        //[DataType(DataType.Custom)]
+        //public string Custom { get; set; } = "dd";
+
+        [DataType(DataType.DateTime)]
+        public DateTime DateTime { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        [DataType(DataType.Time)]
+        public DateTime Time { get; set; }
+
+        [DataType(DataType.Duration)]
+        public DateTime Duration { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal Currency { get; set; }
+
+        [DataType(DataType.Text)]
+        public string Text { get; set; }
+
+        [DataType(DataType.Html)]
+        public string Html { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string MultilineText { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string EmailAddress { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Url)]
+        public string Url1 { get; set; }
+
+        [DataType(DataType.ImageUrl)]
+        public string ImageUrl { get; set; }
+
+        [DataType(DataType.CreditCard)]
+        public string CreditCard { get; set; }
+
+        [DataType(DataType.PostalCode)]
+        public string PostalCode { get; set; }
+
+        [DataType(DataType.Upload)]
+        public string Upload { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ WebApplication1/pages/time.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace WebApplication1.pages
+{
+    public class timeModel : PageModel
+    {
+
+        public string[] Days { get; set; } = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+        [BindProperty]
+        public bool[] DaysChoice { get; set; } = new bool[7];
+
+        public List<SelectListItem> Months = new List<SelectListItem>
+        {
+            new SelectListItem("January","January"),
+            new SelectListItem("February","February"),
+            new SelectListItem("March","March"),
+            new SelectListItem("April","April"),
+            new SelectListItem("May","May"),
+            new SelectListItem("June","June"),
+            new SelectListItem("July","July"),
+            new SelectListItem("August","August"),
+            new SelectListItem("September","September"),
+            new SelectListItem("October","October"),
+            new SelectListItem("November","November"),
+            new SelectListItem("December","December"),
+        };
+
+        [BindProperty]
+        public List<string> ChoosenMonths { get; set; }
+
+        public void OnPost()
+        {
+
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
